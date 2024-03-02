1cad981bd1f3d16b6eb5c48d6ef4800181a08777
Finnbyte <eelpy@protonmail.com>
2024-03-02 17:00:49 +0200

    Make window related data int instead of float

---
+++ components/window/window.go
@@ -0,0 +1,25 @@
+package window
+
+type Window struct {
+	Width, Height int
+}
+
+func (w *Window) HalfWidth() int {
+	return w.Width / 2
+}
+
+func (w *Window) HalfHeight() int {
+	return w.Height / 2
+}
+
+func (w Window) CenterX() int {
+	return w.Width / 2
+}
+
+func (w Window) CenterY() int {
+	return w.Height / 2
+}
+
+var (
+	Win = Window{}
+)
