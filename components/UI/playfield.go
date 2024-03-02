3bcca30f107b5b91a92b3c145f0689647a321ba9
Finnbyte <eelpy@protonmail.com>
2024-03-02 18:13:43 +0200

    Make playfield component use vector strokeLine

--- components/UI/playfield.go
+++ components/UI/playfield.go
@@ -2,31 +2,17 @@ package ui
 
 import (
 	"github.com/hajimehoshi/ebiten/v2"
-	"gong/components/window"
-	"image/color"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+	"golang.org/x/image/colornames"
 )
 
 type Playfield struct {
-	Height        int
-	Window        window.Window
-	Img           *ebiten.Image
-	ImgOptsTop    ebiten.DrawImageOptions
-	ImgOptsBottom ebiten.DrawImageOptions
-}
-
-func (pf *Playfield) Init(color color.Color) {
-	pf.Img = ebiten.NewImage(window.Win.Width, pf.Height)
-	pf.Img.Fill(color)
-
-	// Draw the line across the screen
-	pf.ImgOptsTop = ebiten.DrawImageOptions{}
-	pf.ImgOptsTop.GeoM.Translate(0, 0)
-
-	pf.ImgOptsBottom = ebiten.DrawImageOptions{}
-	pf.ImgOptsBottom.GeoM.Translate(0, float64(window.Win.Height-10))
+	Height int
 }
 
 func (pf *Playfield) Draw(screen *ebiten.Image) {
-	screen.DrawImage(pf.Img, &pf.ImgOptsBottom)
-	screen.DrawImage(pf.Img, &pf.ImgOptsTop)
+	screenWidth := screen.Bounds().Dx()
+	screenHeight := screen.Bounds().Dy()
+	vector.StrokeLine(screen, 0, 0, float32(screenWidth), 0, float32(pf.Height), colornames.White, false)
+	vector.StrokeLine(screen, 0, float32(screenHeight), float32(screenWidth), float32(screenHeight), 2, colornames.White, false)
 }
