11b44ad0652db4421883f15884507ba7fd62336b
Finnbyte <eelpy@protonmail.com>
2024-03-02 19:02:33 +0200

    Change paddle to use vector strokeRect
    Using strokeRect results in hollow, clean looking paddles

--- components/paddle/paddle.go
+++ components/paddle/paddle.go
@@ -1,62 +1,44 @@
 package paddle
 
 import (
-	"gong/components/UI"
-	"gong/components/window"
-	"image/color"
+	ui "gong/components/UI"
+	. "gong/components/screen"
 	"gong/utils"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/vector"
+	"golang.org/x/image/colornames"
 )
 
 const SPEED = 6
 
 type Paddle struct {
-	X         int
-	Y         int
-	playfield ui.Playfield
-	Width     int
-	Height    int
-	velocityY int
-	Img       *ebiten.Image
-	ImgOpts   ebiten.DrawImageOptions
+	X           int
+	Y           int
+	playfield   ui.Playfield
+	StrokeWidth int
+	Width       int
+	Height      int
+	velocityY   int
 }
 
-func (p *Paddle) Init(playfield ui.Playfield, color color.Color) {
-	p.playfield = playfield
-
-	p.Img = ebiten.NewImage(p.Width, p.Height)
-	p.Img.Fill(color)
-
-	// p.Y -= float64(p.Height / 2) // Account for height to center on the Y axis
-	p.ImgOpts.GeoM.Translate(float64(p.X), float64(p.Y))
-}
-
-func (p *Paddle) move() {
-	oldYPos := p.Y
-	minY, maxY := p.playfield.Height-1, window.Win.Height - p.playfield.Height
-
-	newYPos := utils.Clamp(p.Y + p.velocityY, maxY - p.Height, minY)
+func (p *Paddle) update() {
+	minY, maxY := p.playfield.Height-1, Screen.Height-p.playfield.Height
+	newYVelocity := p.Y + p.velocityY
+	newYPos := utils.Clamp(newYVelocity, maxY-p.Height, minY+p.StrokeWidth)
 	p.Y = newYPos
-
-	// Update paddle on screen
-	p.ImgOpts.GeoM.Translate(0, float64(p.Y - oldYPos))
 }
 
 func (p *Paddle) MoveUp() {
 	p.velocityY = -SPEED
-	p.move()
+	p.update()
 }
 
 func (p *Paddle) MoveDown() {
 	p.velocityY = +SPEED
-	p.move()
+	p.update()
 }
 
 func (p *Paddle) Draw(screen *ebiten.Image) {
-	screen.DrawImage(p.Img, &p.ImgOpts)
-}
-
-func (p Paddle) Clear(screen *ebiten.Image) {
-	p.Img.Clear()
+	vector.StrokeRect(screen, float32(p.X), float32(p.Y), float32(p.Width), float32(p.Height), 3, colornames.White, false)
 }
